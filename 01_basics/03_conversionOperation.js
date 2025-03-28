bc9ebb195e800a375fc7924135c5270d98f43d52
Anita Khadka <144998659+02052023@users.noreply.github.com>
2025-03-28 08:46:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//let score = 33; // trial (result number) 
+// let score = "33"; // trial (result string) 
+// let score = "33abc"; // trial (result string) trial 3 result NaN
+// let score = null (reult 0)
+// // let score = undefined // (reult 0)
+// let score = "Anita" // (reult it cant change in no NaN)
+// // always chq type of to know data type
+
+// console.log(typeof score);//typeof
+// console.log(typeof (score));//method
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // string converted to  number
+// console.log(valueInNumber);
+
+// let isLogggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanIsLoggedIn); (true)
+
+// let isLogggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanIsLoggedIn); false
+// let isLogggedIn = "Anita"
+// let booleanIsLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanIsLoggedIn); true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
