471481adb5a8c49c004805bff2079e4c0e35610c
Anita Khadka <144998659+02052023@users.noreply.github.com>
2025-04-02 06:54:26 +0000

    datatype summary

---
+++ 01_basics/summarydatatypes.js
@@ -0,0 +1,34 @@
+//Primitive
+
+// 7 types : String,Number, Boolean, null,undefined,Symbol, Bigint
+
+const score  = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+//const bigNumber = 1243345676857658355n
+
+
+//Reference type (non-primitive)
+
+//Array, Objects(imp browsere web events ,evennts),function
+
+const heros = ["salman","amir"]
+let myObj ={
+    name:"anita"
+    // age: 22,
+}
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof bigNumber);
