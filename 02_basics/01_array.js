330d3c829710fee46dd2a6bb1cd3df76283aaf8b
Anita Khadka <144998659+02052023@users.noreply.github.com>
2025-04-16 05:31:07 +0000

    01_arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// arry
+
+const myArr = [0,1,2,3,4,5]// arry square brackets ke andr rehta hai //inside that elements  //array is a object
+
+console.log(myArr[2]);//it always strt with 0 index//shallow copy
+
+//Array Method 
+
+// myArr.push(6)
+// myArr.push(7) // to add value
+// myArr.pop() // to clear last value
+
+myArr.unshift(9)
+myArr.shift()
+
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+
+// slice , splice
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B" , myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C" , myArr);
+
+console.log(myn1);
+console.log(myn2);
